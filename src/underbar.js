c4a36e203ddad193b705b988014a18b25c9dc9c9
Brandon Tiqui <btiqui@gmail.com>
2016-06-26 20:00:52 -0700

    Refactor the map function with ES6 arrow functions

--- src/underbar.js
+++ src/underbar.js
@@ -121,7 +121,7 @@
 
 
   // Return the results of applying an iterator to each element.
-  _.map = function(collection, iterator) {
+  _.map = (collection, iterator) => {
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
