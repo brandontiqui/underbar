31e040e6208822dac00846335d7d53ffd8693138
Brandon Tiqui <btiqui@gmail.com>
2016-06-26 20:10:34 -0700

    Refactor the every function with ES6 arrow functions

--- src/underbar.js
+++ src/underbar.js
@@ -203,7 +203,7 @@
 
 
   // Determine whether all of the elements match a truth test.
-  _.every = function(collection, iterator) {
+  _.every = (collection, iterator) => {
     // TIP: Try re-using reduce() here.
     if (iterator === undefined) {
       return !_.contains(collection, false);
